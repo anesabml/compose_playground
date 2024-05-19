37e84247c3dc29e96b5553aa7293179e387a7c68
Anes Abismail <anes.abml@gmail.com>
2024-05-19 18:35:12 +0100

    Initial commit

---
+++ app/src/main/kotlin/io/anesabml/composeplayground/ui/CustomCircleProgress.kt
@@ -0,0 +1,125 @@
+package io.anesabml.composeplayground.ui
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.Morph
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.circle
+import androidx.graphics.shapes.star
+import androidx.graphics.shapes.toPath
+
+@Composable
+fun SquigglyCircleProgressBar() {
+    val starPolygon = remember {
+        RoundedPolygon.star(
+            numVerticesPerRadius = 12,
+            innerRadius = 2f / 3f,
+            rounding = CornerRounding(1f / 6f)
+        )
+    }
+
+    val circlePolygon = remember {
+        RoundedPolygon.circle(
+            numVertices = 12
+        )
+    }
+
+    val morph = remember {
+        Morph(starPolygon, circlePolygon)
+    }
+
+    val infiniteTransition = rememberInfiniteTransition(label = "shape")
+    val progress = infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            tween(4000, easing = LinearEasing),
+            RepeatMode.Reverse
+        ),
+        label = "progress"
+    )
+
+    val rotation = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            tween(4000, easing = LinearEasing),
+            RepeatMode.Reverse
+        ),
+        label = "rotation"
+    )
+    val pathMeasure = remember { PathMeasure() }
+    var androidPath = remember { android.graphics.Path() }
+    var composePath = remember { Path() }
+    val destinationPath = remember { Path() }
+    val matrix = remember { Matrix() }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .drawWithCache {
+                // TODO investigate performance hits if androidPath is not specified
+                androidPath = morph.toPath(progress.value, androidPath)
+                composePath = androidPath.asComposePath()
+                matrix.reset()
+                matrix.scale(size.minDimension / 2f, size.minDimension / 2f)
+                composePath.transform(matrix)
+
+                pathMeasure.setPath(composePath, false)
+                destinationPath.reset()
+                pathMeasure.getSegment(0f, pathMeasure.length * progress.value, destinationPath)
+
+                onDrawBehind {
+                    rotate(rotation.value) {
+                        translate(size.width / 2f, size.height / 2f) {
+                            val brush = Brush.sweepGradient(colors, Offset(0.5f, 0.5f))
+                            drawPath(destinationPath, brush = brush, style = Stroke(16.dp.toPx(), cap = StrokeCap.Round))
+                        }
+                    }
+                }
+            }
+    )
+}
+
+private val colors = listOf(
+    Color(0xFF3FCEBC),
+    Color(0xFF3CBCEB),
+    Color(0xFF5F96E7),
+    Color(0xFF816FE3),
+    Color(0xFF9F5EE2),
+    Color(0xFFBD4CE0),
+    Color(0xFFDE589F),
+    Color(0xFF3FCEBC),
+)
+
+@Preview(showBackground = true, device = "id:pixel_5")
+@Composable
+fun PreviewSquigglyCircleProgressBar() {
+    SquigglyCircleProgressBar()
+}
---
+++ app/src/main/kotlin/io/anesabml/composeplayground/ui/InfiniteLoader.kt
@@ -0,0 +1,162 @@
+package io.anesabml.composeplayground.ui
+
+import android.R.attr
+import android.R.attr.animation
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+data class Glow(
+    val radius: Dp = 8.dp,    // Controls glow size
+    val xShifting: Dp = 0.dp, // Adjusts horizontal position
+    val yShifting: Dp = 0.dp  // Adjusts vertical position
+)
+
+@Composable
+fun InfiniteLoader(
+    brush: Brush,
+    modifier: Modifier = Modifier,
+    duration: Int = 3_000,
+    strokeWidth: Dp = 4.dp,
+    strokeCap: StrokeCap = StrokeCap.Round,
+    glow: Glow? = null,
+    placeholderColor: Color? = null,
+) {
+    val infiniteTransition = rememberInfiniteTransition("PathTransition")
+    val pathCompletion by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(duration, easing = LinearEasing)
+        ),
+        label = "Progress"
+    )
+
+    Canvas(modifier = modifier) {
+        val path = createInfiniteShape(size.width, size.height)
+
+        val paint = setupPaint(strokeWidth, strokeCap, brush)
+        glow?.applyToPaint(paint, this)
+        placeholderColor?.let { color -> drawPathPlaceholder(path, strokeWidth, strokeCap, color) }
+
+        val pathSegment = calculatePathSegment(path, pathCompletion)
+        drawPathSegment(pathSegment, paint)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InfiniteLoaderPreview() {
+    InfiniteLoader(
+        brush = Brush.horizontalGradient(
+            colors = listOf(Color.Red, Color.Blue)
+        ),
+        modifier = Modifier
+            .width(200.dp)
+            .height(150.dp),
+        glow = Glow(),
+        placeholderColor = Color.Black.copy(.16f)
+    )
+}
+
+private fun DrawScope.drawPathSegment(pathSegment: Path, paint: Paint) {
+    drawIntoCanvas { canvas -> canvas.drawPath(pathSegment, paint) }
+}
+
+private fun DrawScope.drawPathPlaceholder(
+    path: Path,
+    strokeWidth: Dp,
+    strokeCap: StrokeCap,
+    placeholderColor: Color
+) {
+    drawPath(
+        path = path,
+        color = placeholderColor,
+        style = Stroke(
+            width = strokeWidth.toPx(),
+            cap = strokeCap
+        )
+    )
+}
+
+private fun Glow.applyToPaint(
+    paint: Paint,
+    density: Density
+) = with(density) {
+    val frameworkPaint = paint.asFrameworkPaint()
+    frameworkPaint.setShadowLayer(
+        radius.toPx(),
+        xShifting.toPx(),
+        yShifting.toPx(),
+        android.graphics.Color.WHITE
+    )
+}
+
+private fun DrawScope.setupPaint(
+    strokeWidth: Dp,
+    strokeCap: StrokeCap,
+    brush: Brush
+): Paint = Paint().apply {
+    this.isAntiAlias = true
+    this.style = PaintingStyle.Stroke
+    this.strokeWidth = strokeWidth.toPx()
+    this.strokeCap = strokeCap
+
+    brush.applyTo(size, this, 1f)
+}
+
+private fun calculatePathSegment(path: Path, pathCompletion: Float): Path {
+    val pathMeasure = PathMeasure().apply { setPath(path, false) }
+
+    val startDistance = when {
+        (pathCompletion > 1f) -> ((pathCompletion - 1f) * pathMeasure.length)
+        else -> 0f
+    }
+
+    val stopDistance = when {
+        (pathCompletion < 1f) -> (pathCompletion * pathMeasure.length)
+        else -> pathMeasure.length
+    }
+
+    val pathSegment = Path()
+    pathMeasure.getSegment(startDistance, stopDistance, pathSegment, true)
+    return pathSegment
+}
+
+private fun createInfiniteShape(width: Float, height: Float): Path =
+    Path().apply {
+        moveTo((width / 2), (height / 2))
+        cubicTo(
+            x1 = width, y1 = 0f,
+            x2 = width, y2 = height,
+            x3 = (width / 2), y3 = (height / 2)
+        )
+        cubicTo(
+            x1 = 0f, y1 = 0f,
+            x2 = 0f, y2 = height,
+            x3 = (width / 2), (height / 2)
+        )
+    }
